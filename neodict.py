0c589709cef7154876e0b170f78590ea62b5265d
Branchey <70140890+CZboop@users.noreply.github.com>
2022-02-23 19:26:41 +0000

    added skip word functionality and started adding more logic towards the main definition functionality

--- neodict.py
+++ neodict.py
@@ -71,103 +71,22 @@ class neoDict(MDApp):
         #returning the loaded app with screen manager root
         return kv_file
 
-    def sample(self, preds, temperature=1.0):
-        preds = np.asarray(preds).astype("float64")
-        preds = np.log(preds) / temperature
-        exp_preds = np.exp(preds)
-        preds = exp_preds / np.sum(exp_preds)
-        probas = np.random.multinomial(1, preds, 1)
-        return np.argmax(probas)
-
-    def setup_model(self):
-        # TODO: TRY OUT STARTING TO ADAPT THE SCRIPT TO RUN HERE AND SEE HOW THAT GOES
-        # seems to basically work in the background but while that's happening, the app doesn't respond
-        # try workaround or can always pregenerate loads and have that in a file that gets fed in ...
-        adjs_df = pd.read_csv('Adjectives.csv',  usecols=[1], header = 0, delimiter=",", quoting=csv.QUOTE_NONE,
-                               encoding='utf-8')
-        adjs_df.columns = ['word']
-
-        adjs = adjs_df.word.tolist()
-        adjs = [i.replace(" ", "") for i in adjs]
-        text = " ".join(adjs).lower().replace("\"", "")
-        print(text[:100], len(text))
-
-        chars = sorted(list(set(text)))
-        # print(chars)
-
-        # making dicts for encoding of characters
-        char_indices = dict((v, c) for c,v in enumerate(chars))
-        indices_char = dict((c, v) for c,v in enumerate(chars))
-
-        maxlen = 30
-        step = 3
-        sequences = []
-        next_char = []
-
-        for i in range(0, len(text) - maxlen, step):
-          sequences.append(text[i : i + maxlen])
-          next_char.append(text[i + maxlen])
-
-        x = np.zeros((len(sequences), maxlen, len(chars)), dtype=np.bool)
-        y = np.zeros((len(sequences), len(chars)), dtype=np.bool)
-        #
-        for i, sequence in enumerate(sequences):
-            for t, char in enumerate(sequence):
-                x[i, t, char_indices[char]] = 1
-            y[i, char_indices[next_char[i]]] = 1
-
-        model = keras.models.load_model('adj_generate_model.h5', custom_objects=None, compile=True, options=None)
-
-        epochs = 40
-        batch_size = 128
-
-        for epoch in range(epochs):
-            model.fit(x, y, batch_size=batch_size, epochs=1)
-            print("Generating... \n Epoch: " + str(epoch))
-
-            start_index = random.randint(0, len(text) - maxlen - 1)
-            for diversity in [0.2, 0.5, 1.0, 1.2]:
-                generated = ""
-                sentence = text[start_index : start_index + maxlen]
-
-                for i in range(400):
-                    x_pred = np.zeros((1, maxlen, len(chars)))
-                    for t, char in enumerate(sentence):
-                        x_pred[0, t, char_indices[char]] = 1.0
-                    preds = model.predict(x_pred, verbose=0)[0]
-                    next_index = self.sample(preds, diversity)
-                    next_char = indices_char[next_index]
-                    sentence = sentence[1:] + next_char
-                    generated += next_char
-                # print("Generated: ", generated)
-
-                with open("generated_adjs.txt", "a") as txt:
-                  # tend to get a lot of repeats for some seeds, filter out when reading back the file
-                  txt.write(generated + " ")
-        model.save('adj_generate_model.h5')
+    def load_words(self):
+        #load the file, get words into a list and start sending to app or
         generated_words = open('generated_adjs.txt', 'r')
         generated_words = generated_words.read()
 
-        words_list = list([i for i in set(generated_words.split(" ")) if len(i) > 0])
+        self.words_list = list([i for i in set(generated_words.split(" ")) if len(i) > 0])
+        random.shuffle(self.words_list)
 
-        # rewriting the file without repeated words
-        with open("generated_adjs.txt", "w") as txt:
-          for i in words_list:
-                  txt.write(i + "\n")
+        self.definitions_dict = {}
 
-        # print(words_list)
-        definitions_dict = {}
+    def set_word(self):
+        self.prompt_word = self.words_list[0]
+        prompt_text = "What does \n {} \n mean?".format(self.prompt_word)
+        self.root.get_screen('Main').ids.prompttext.text = prompt_text
+        # and add a skip word button, exit button?, and text input to ui
 
-        for word in words_list:
-          definition = input("What does {} mean?\n".format(word))
-          if definition == "exit":
-            break
-          if definition == "skip":
-            continue
-          else:
-            definitions_dict[word] = [definition.capitalize()]
-            example = input("What is an example of {} in a sentence?\n".format(word))
-            definitions_dict[word].append(example.capitalize())
 
     # convert to full dictionary entry
 
@@ -185,6 +104,18 @@ class neoDict(MDApp):
 
         with open(filename + ".txt", "a") as txt:
                 txt.write(entry + "\n\n")
+
+    def submit(self, definition):
+        # switch text to ask for example in a sentence, maybe have a different screen
+        self.root.get_screen('Main').ids.prompttext.text = 'Ok. And what is an example of {} in a sentence'.format(self.prompt_word)
+        self.state = example
+        # could also do
+
+
+    def skip(self):
+        self.words_list = self.words_list[1:]
+        self.set_word()
+
 
 
 # running the app
